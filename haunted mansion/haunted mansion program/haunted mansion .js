22f9614f130d53caeadd100f12543d9ae109b27a
aidan <aidanhoward757@gmail.com>
2022-01-31 09:01:19 -0800

    all work from thursday

--- haunted mansion/haunted mansion program/haunted mansion .js
+++ haunted mansion/haunted mansion program/haunted mansion .js
@@ -1,6 +1,26 @@
 /*
-programmer: aidan howard
-team member: vance
-date: 1.26.2022
-program: haunted mansion maze using java script
+Programmer: Vance Clark
+Team Member: Aidan Howard
+Date: 1.26.2022
+Program: We will be making a program to go through a Haunted Mansion Maze filled with Haunted Sounds, Changing of colors, and other obstacles along the way using a Sphero Bolt and Java Script
 */
+
+async function startProgram() {
+	// Write code here
+	// Says start and rolls the first stretch
+
+	await speak('Start', true)
+	await roll(0,60,2);
+	
+	// Set LED blue and goes to sound checkpoint 
+	
+	setMainLed({ r: 0, g: 0, b: 225 })
+	await roll(50, 30, 2)
+	await roll(90, 30, 2)
+	
+	// Plays monkey sound and goes to red checkpoint
+
+	await Sound.Animal.Monkey.play(true)
+
+	
+}
